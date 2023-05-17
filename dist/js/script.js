006c3d1d23fbf6cd58a3677ed11921e71266d8fc
Dominik Domański <dominikd33@o2.pl>
2023-05-17 17:45:05 +0200

    add price calc

--- dist/js/script.js
+++ dist/js/script.js
@@ -163,15 +163,27 @@ const select = {
 
       // for every category (param)...
       for(let paramId in thisProduct.data.params) {
+
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
         console.log(paramId, param);
 
         // for every option in this category
         for(let optionId in param.options) {
+
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           console.log(optionId, option);
+
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+
+            // check if the option is not default
+            if(option !== 'default') {
+              price += option.price;
+            } else {
+              price -= option.price;
+            }
+          }
         }
       }
 
